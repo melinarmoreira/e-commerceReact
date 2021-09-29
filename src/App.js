0bb6ce4dc733460278e6daad83cf4172b59e9745
Melina Moreira <melina.moreira@live.com.ar>
2021-09-29 00:26:28 -0300

    Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 
 import "./App.css";
 import NavBar from "./components/NavBar";
-// import CartWidget from "./components/CartWidget";
 import ItemListContainer from "./components/ItemListContainer";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
